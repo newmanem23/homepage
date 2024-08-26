c13b27c1804124e09a334f163acb4229816cfa37
Ed Newman <newmanem23@gmail.com>
2024-08-25 23:35:36 -0400

    Break out enter handler / typing div
    The previous code had an event listener on an input just for enter. I
    broke it into its own handler and began setting up typing in the new
    text div.

--- js/main.js
+++ js/main.js
@@ -3,40 +3,52 @@ import executors from "./executors.js";
 import { error, render } from "./helpers.js";
 import shortcuts from "./shortcuts.js";
 
-const input = document.getElementById("input");
+const input = document.querySelector(".text");
 const output = document.getElementById("output");
 // TODO: Add autosuggest / autocomplete functionality
 
-input.addEventListener("keydown", function (e) {
+addEventListener("keydown", function (e) {
   // TODO: Refactor this to use a div rather than the input. We want to have the cursor block vs. the blinking cursor.
   if (e.key === "Enter") {
-    const userInput = input.value.trim().split(" ");
-    const command = userInput[0].toLowerCase();
-    const options = userInput.slice(1);
-    render(`<span class="green">❯&nbsp;</span>${input.value}`);
-    try {
-      const commandDetails = commands.find((c) =>
-        c.name.map((n) => n.toLowerCase()).includes(command),
-      );
-      if (commandDetails) {
-        if (command === "help") commandDetails.execute(commands);
-        else commandDetails.execute(options);
-      } else {
-        const shortcutDetails = shortcuts
-          .flatMap((c) => Object.entries(c.items))
-          .find(([i]) => i.toLowerCase().startsWith(command));
-        if (shortcutDetails) {
-          console.log(shortcutDetails);
-          render(`Redirecting to ${shortcutDetails[0]}...`);
-          window.location.href = shortcutDetails[1];
-        } else error("yellow", command, "command not found");
-      }
-    } catch (e) {
-      error("red", "JS Error", e.message);
-    }
-    input.value = "";
+    enterHandler();
+    return;
+  }
+  if (isLetter(e.key)) {
+    input.innerText = input.innerText + e.key;
   }
 });
+
+const enterHandler = () => {
+  const userInput = input.innerText.trim().split(" ");
+  const command = userInput[0].toLowerCase();
+  const options = userInput.slice(1);
+  render(`<span class="green">❯&nbsp;</span>${input.value}`);
+  try {
+    const commandDetails = commands.find((c) =>
+      c.name.map((n) => n.toLowerCase()).includes(command),
+    );
+    if (commandDetails) {
+      if (command === "help") commandDetails.execute(commands);
+      else commandDetails.execute(options);
+    } else {
+      const shortcutDetails = shortcuts
+        .flatMap((c) => Object.entries(c.items))
+        .find(([i]) => i.toLowerCase().startsWith(command));
+      if (shortcutDetails) {
+        console.log(shortcutDetails);
+        render(`Redirecting to ${shortcutDetails[0]}...`);
+        window.location.href = shortcutDetails[1];
+      } else error("yellow", command, "command not found");
+    }
+  } catch (e) {
+    error("red", "JS Error", e.message);
+  }
+  input.value = "";
+};
+
+const isLetter = (letter) => {
+  return /^[a-zA-Z]$/.test(letter);
+};
 
 window.addEventListener("load", () => {
   executors.ls();
