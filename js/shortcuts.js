5c63d103b790df2b6ef4f8193604daccb06018d7
Ed Newman <newmanem23@gmail.com>
2024-08-26 16:51:08 -0400

    Add Odin Project shortcut

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -16,7 +16,7 @@ export default [
     color: "yellow",
     items: {
       Github: "https://github.com",
-      Github: "https://github.com",
+      "The Odin Project": "https://www.theodinproject.com/dashboard",
       "Frontend Masters": "https://frontendmasters.com/dashboard/",
     },
   },
