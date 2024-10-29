370419deaf76c55011d938834d41b60d181ac0d0
Ed Newman <newmanem23@gmail.com>
2024-10-28 21:36:27 -0400

    Revise jira dashboard link

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -33,7 +33,7 @@ export default [
       Confluence: "https://talentexp.atlassian.net/wiki/home",
       ChatGPT: "https://chatgpt.com/?oai-dm=1",
       "Jira Dashboard":
-        "https://phenompeople.atlassian.net/issues/?filter=49260",
+        "https://phenompeople.atlassian.net/jira/dashboards/20149",
       "Active Projects":
         "https://docs.google.com/spreadsheets/d/1-e1Vy1qpcAMOdH7jokes7G6-uIPgdR2y06G7nihgCeI/edit?gid=1294137478#gid=1294137478",
       Smartsheet: "https://app.smartsheet.com/b/home",
