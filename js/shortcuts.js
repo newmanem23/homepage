c0374511f748f730a841a73d6c38790d0fb39551
Ed Newman <newmanem23@gmail.com>
2025-04-15 13:25:32 -0400

    Remove zillow link

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -54,7 +54,6 @@ export default [
         "https://247sports.com/college/michigan/Board/The-Victors-Club-102410",
       "Newman Wordle":
         "https://docs.google.com/spreadsheets/d/1T99BdVtCdP4_ySerAjWei5tIUFdnANIimp1rDYf5PQ8/edit?gid=1613222831#gid=1613222831",
-      Zillow: "https://www.zillow.com",
       Crossword: "https://downforacross.com/",
     },
   },
